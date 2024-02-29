e60ea91b33f747037a1a863fa76955c522691de6
rahulkumar1709 <149097777+rahulkumar1709@users.noreply.github.com>
2024-02-29 10:46:56 +0000

    dataTypes

---
+++ 01_basics/dataTypes.js
@@ -0,0 +1,9 @@
+
+"use strict"; // trial all js code as newer version
+//alert("hello") we are using node js not browser
+let name="" //string
+let age=18 //int
+let isloggedIn=true //boolean
+//null=standAlone value    it is a object
+//undefined=
+//symbol=unique
---
+++ 01_basics/variables.js
@@ -0,0 +1,11 @@
+const accountId=74784
+let accountEmail="rahulkumarofficial.gmail.com"
+var accountPassword="784848"
+accountCity="jaipur"
+//accountId=89448// not allowed
+accountEmail="krahul@gmail.com"
+accountCity="bengaluru"
+console.log(accountEmail)
+console.table([accountCity,accountEmail,accountPassword])
+//prefer not to use var because of  issue in block scope and functional scope//
+
