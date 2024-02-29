ea0a6da61fd29783b5ed2f9160d3729c5655c7b4
rahulkumar1709 <149097777+rahulkumar1709@users.noreply.github.com>
2024-02-29 12:02:32 +0000

    conversion of types

---
+++ 01_basics/conversionOperation.js
@@ -0,0 +1,10 @@
+let score="89"
+console.log(typeof score);
+let ValueInNumber=Number(score)
+console.log(typeof ValueInNumber)
+console.log(ValueInNumber)
+
+//note "89"=>33
+//"33abc"=>NaN
+//true=>1;false=>0
+//""=0
