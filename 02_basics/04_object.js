45a6d90cd73c4c67d004bbfd61507751f35b3829
rahulkumar1709 <149097777+rahulkumar1709@users.noreply.github.com>
2024-03-25 16:29:48 +0000

    json

---
+++ 02_basics/04_object.js
@@ -0,0 +1,17 @@
+const course={
+    coursename:"js in hindi",
+    price:"999",
+    courseInstructor:"hitesh",
+}
+console.log(course.courseInstructor);
+console.log(course["courseInstructor"])
+const{courseInstructor:instructor}=course
+console.log(courseInstructor);
+console.log(instructor);
+
+{
+   "name" :"hitesesh",
+   "coursename":"js in hindi",
+   "price":"free"
+
+}
