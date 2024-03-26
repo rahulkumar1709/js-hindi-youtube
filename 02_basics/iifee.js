5a986ac22e9a55cee51ffffbda80add24a802fae
rahulkumar1709 <149097777+rahulkumar1709@users.noreply.github.com>
2024-03-26 20:09:26 +0000

    iifee

---
+++ 02_basics/iifee.js
@@ -0,0 +1,25 @@
+//immediately invoked function expression(IIFE)
+
+function chai(){
+    console.log(`DB connected`);
+}chai()
+
+//both are same but second one is iifee
+(function chai(){
+    //named iifee
+    console.log(`DB connected`);
+})();
+//syntax ()()
+
+
+( () =>{
+    console.log(`db is connected`);
+})();
+
+
+( (name) =>{
+    //parmater passing in iifee
+    console.log(`db is connected ${name}`);
+})('rahul')
+
+
