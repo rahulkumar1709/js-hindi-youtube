5011c43311c473003f6a290bbda9561c1ae4fb29
rahulkumar1709 <149097777+rahulkumar1709@users.noreply.github.com>
2024-03-30 18:52:44 +0000

    filter

---
+++ 03_iteration/five.js
@@ -0,0 +1,26 @@
+// const coding=["js","ruby","java","python","cpp"]
+// const value=coding.forEach((item)=>{
+// console.log(item);
+// })
+// console.log(value);
+//in foreach loop there is no return value means you cant take a single value of a array or 
+//or check or compare with the other value
+
+
+
+const myNums=[1,2,3,4,5,6,7,8,9,10]
+// const newNums=myNums.filter((num)=>{
+//    return num>4
+// })
+// console.log(newNums);
+
+//2nd approach to do the same 
+//  const newNums=[]
+//  myNums.filter((num)=>{
+//     if(num>4){
+//         newNums.push(num)
+//     }
+//  })
+//  console.log(newNums);
+
+
